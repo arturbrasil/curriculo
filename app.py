6244769e460f6fd0a083c0908f22a4fa43d57fb8
Artur Brasil <arturbrasil@gmail.com>
2024-01-26 20:59:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
+from transformers import AutoTokenizer, Wav2Vec2ForCTC
 import torchaudio
 import torch
 import os
@@ -9,9 +9,9 @@ app = FastAPI()
 
 os.makedirs("temp", exist_ok=True)
 
-# Carregando o modelo Wav2Vec2 pré-treinado e o processor
-model_name = 'lgris/wav2vec2-large-xlsr-open-brazilian-portuguese-v2'
-processor_asr = Wav2Vec2Processor.from_pretrained(model_name)
+# Carregando o modelo Wav2Vec2 pré-treinado e o tokenizer
+model_name = 'Edresson/wav2vec2-large-xlsr-coraa-portuguese'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 modelo_asr = Wav2Vec2ForCTC.from_pretrained(model_name)
 
 @app.post("/asr")
@@ -24,13 +24,13 @@ async def transcrever_audio(file: UploadFile = File(...)):
         # Carrega o áudio usando torchaudio
         waveform, sample_rate = torchaudio.load(caminho_temporario)
 
-        # Pré-processamento usando o processor do modelo Wav2Vec2
-        input_values = processor_asr(waveform.numpy(), return_tensors="pt").input_values
+        # Pré-processamento usando o tokenizer do modelo Wav2Vec2
+        input_values = tokenizer(waveform.squeeze().numpy(), return_tensors="pt", padding="longest").input_values
 
         # Inferência do modelo
         with torch.no_grad():
             logits = modelo_asr(input_values).logits
-            transcription = processor_asr.batch_decode(logits.argmax(dim=-1))[0]
+            transcription = tokenizer.batch_decode(logits.argmax(dim=-1))[0]
 
         os.remove(caminho_temporario)
 
@@ -43,9 +43,6 @@ async def transcrever_audio(file: UploadFile = File(...)):
 # ...
 
 # Teste contra Common Voice (Domínio)
-# ...
-
-# Teste contra TEDx (Fora do Domínio)
 # ...
 
 if __name__ == "__main__":
